08cb6acbcbbba7bca583eecefa8552ca61572e60
pmadi <36573109+pmadi@users.noreply.github.com>
2019-06-05 00:56:23 +0900

    Update makeseeds.py

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -12,8 +12,8 @@ MIN_BLOCKS = 200000
 # These are hosts that have been observed to be behaving strangely (e.g.
 # aggressively connecting to every node).
 SUSPICIOUS_HOSTS = set([
-    "127.0.0.1", "165.227.29.42",
-    "138.68.235.25", "178.128.11.178"
+    "127.0.0.1", "167.99.229.138",
+    "157.230.231.118", "142.93.9.180"
 ])
 
 import re
