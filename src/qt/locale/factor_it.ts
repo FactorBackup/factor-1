3c4a97acccdf7bce027dab30ad41dcb7c5fb263f
pmadi <pmadi0140@gmail.com>
2019-06-05 08:32:39 +0900

    master
    master

--- src/qt/locale/factor_it.ts
+++ src/qt/locale/factor_it.ts
@@ -846,15 +846,15 @@ Indirizzo: %4
     </message>
     <message>
         <source>This option is the quickest and will cost about ~0.025 FACTOR to anonymize 1000 FACTOR</source>
-        <translation>Questa opzione è la più veloce e ti costerà ~0.025DRK circa per anonimizzare 1000DRK</translation>
+        <translation>Questa opzione è la più veloce e ti costerà ~0.025FTC circa per anonimizzare 1000FTC</translation>
     </message>
     <message>
         <source>This option is moderately fast and will cost about 0.05 FACTOR to anonymize 1000 FACTOR</source>
-        <translation>Questa opzione è moderatamente veloce e costerà 0.05DRK circa per anonimizzare 1000DRK</translation>
+        <translation>Questa opzione è moderatamente veloce e costerà 0.05FTC circa per anonimizzare 1000FTC</translation>
     </message>
     <message>
         <source>0.1 FACTOR per 1000 FACTOR you anonymize.</source>
-        <translation>0.1DRK ogni 1000DRK anonimizzati.</translation>
+        <translation>0.1FTC ogni 1000FTC anonimizzati.</translation>
     </message>
     <message>
         <source>This is the slowest and most secure option. Using maximum anonymity will cost</source>
